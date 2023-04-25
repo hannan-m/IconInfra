f87e7105ad2ffd2a0979396e8c824d53ed1a0056
Hannan <hannansultan@outlook.com>
2023-04-25 05:36:03 +0500

    VM and realted resources creation

--- main.tf
+++ main.tf
@@ -24,3 +24,83 @@ resource "azurerm_storage_container" "production" {
   storage_account_name  = azurerm_storage_account.testsa.name
   container_access_type = "private"
 }
+
+# Create virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "${var.prefix}-vm-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.testrg.location
+  resource_group_name = azurerm_resource_group.testrg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "${var.prefix}-vm-subnet"
+  resource_group_name  = azurerm_resource_group.testrg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "${var.prefix}-vm-ip"
+  location            = azurerm_resource_group.testrg.location
+  resource_group_name = azurerm_resource_group.testrg.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "${var.prefix}-vm-nsg"
+  location            = azurerm_resource_group.testrg.location
+  resource_group_name = azurerm_resource_group.testrg.name
+
+}
+
+# Create network interface
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "${var.prefix}-vm-nic"
+  location            = azurerm_resource_group.testrg.location
+  resource_group_name = azurerm_resource_group.testrg.name
+
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
+
+# Create virtual machine
+resource "azurerm_windows_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  admin_username        = "azureuser"
+  admin_password        = "Malta-2023#@!"
+  location              = azurerm_resource_group.testrg.location
+  resource_group_name   = azurerm_resource_group.testrg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+
+  # well suited for application that benefits from low latency and high speed local storage
+  # Standard_D4lds_v5 can be used in case more CPU, RAM and storage required, 140$/month
+  size = "Standard_D2lds_v5" # 70$/month
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+}
