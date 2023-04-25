c850c6187616872f742bc5396b6036c80976c22b
Hannan <hannansultan@outlook.com>
2023-04-25 05:36:52 +0500

    DB server and databses creation

--- main.tf
+++ main.tf
@@ -104,3 +104,38 @@ resource "azurerm_windows_virtual_machine" "main" {
     version   = "latest"
   }
 }
+
+
+# Databse creation
+resource "azurerm_mssql_server" "dbserver" {
+  name                         = "${var.prefix}-sqlserver"
+  resource_group_name          = azurerm_resource_group.testrg.name
+  location                     = azurerm_resource_group.testrg.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+
+}
+
+#Creating DB instances
+resource "azurerm_mssql_database" "db_staging_instance" {
+  name           = "db_staging_instance"
+  server_id      = azurerm_mssql_server.dbserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = false
+}
+
+resource "azurerm_mssql_database" "db_production_instance" {
+  name           = "db_production_instance"
+  server_id      = azurerm_mssql_server.dbserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 100
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+}
